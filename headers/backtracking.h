f107a12338ac8199f283e38f041604190198297d
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-11-19 18:41:52 -0300

    Basic Functions added

---
+++ headers/backtracking.h
@@ -0,0 +1,13 @@
+#ifndef _BACKTRACKING_H
+#define _BACKTRACKING_H
+
+#define SIN_INGRESO -1
+#define BRUTE_FORCE 1
+#define ABOUT 2
+#define EXIT 3
+
+void initBacktracking();
+void runBacktracking();
+void printCurrent();
+
+#endif
